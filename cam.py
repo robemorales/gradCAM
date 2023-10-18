67966506ec39b42f18f8b1523b2e9ba5781220f3
robemorales <75325910+robemorales@users.noreply.github.com>
2023-10-18 18:44:35 -0300

    Update cam.py

--- cam.py
+++ cam.py
@@ -117,6 +117,7 @@ if __name__ == '__main__':
                                     mean=[0.485, 0.456, 0.406],
                                     std=[0.229, 0.224, 0.225],
                                     size=224)
+    print("este es el tensor: ",input_tensor)
     
 
     # Obtener la imagen inversa
